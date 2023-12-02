03202a28fa3e98cec2f257ea2a579b2cf574411b
R.O.O.T.S <152678765+Rootsinc23@users.noreply.github.com>
2023-12-02 01:30:24 -0500

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class Parent {
+	public String firstName;
+	public String dateOfBirth;
+	public boolean gender;
+	public String bloodGroup;
+	public String nationality;
+	public String category;
+	public String Religion;
+}
